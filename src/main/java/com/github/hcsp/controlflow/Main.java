b3091f6746393a7c7d89c3de42bc150a437f3ee2
buuoltwo <bullnick20@gmail.com>
2024-07-25 16:50:08 +0800

    style

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,8 +12,8 @@ public class Main {
     public static int countUpperCaseLetters(String str) {
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
-                count ++;
+            if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
+                count++;
             }
         }
         System.out.println("count = " + count);
